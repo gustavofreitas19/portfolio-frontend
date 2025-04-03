df8183379d7de7dd3a02aac2251ec84d03a2f780
Gustavo De Freitas Cardoso <gustavo.275323@alunosatc.edu.br>
2025-04-02 21:31:02 -0300

    javascript

---
+++ javascript/index.js
@@ -0,0 +1,26 @@
+const Times = [
+    {nome: "Flamengo" , cidade:" Rio de Janeiro"},
+    {nome: "Internacional" , cidade:"Porto Alegre"},
+    {nome: "Bahia" , cidade:" Bahia"}
+
+];
+
+const Partidas = [
+    {casa:"Flamengo",visitante:"Internacional"}
+]
+
+/*
+for(let i = 0 ; i < 3; i ++) {
+    let name = prompt ("digite o nome do seu time: ")
+    let city = prompt ("digite o nome da sua cidade: ")
+
+    Times.punch({
+        name,city
+    })
+
+}
+    */
+
+for(let partida of partidas){
+    console.log(Time)
+}
